cf2cde791031bcd63d2dde84d01c8ea9aaf3f665
Luluh97 <luluadel.97@gmail.com>
2019-10-28 20:14:35 +0300

    commit

---
+++ Java/Fundamentals/Pythagorean.java
@@ -0,0 +1,11 @@
+// import java.lang;
+public class Pythagorean {
+    public double calculateHypotenuse(double legA, double legB) {
+        double a = legA*legA;
+        double b = legB*legB;
+
+        return Math.sqrt(a+b);
+    }
+}
+
+
---
+++ Java/Fundamentals/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean ans = new Pythagorean();
+        double answer = ans.calculateHypotenuse(2.0,4.0);
+        System.out.println(answer);
+    }
+}
