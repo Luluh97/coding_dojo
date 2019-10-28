3e2f21698725e965a2552502bebf341aeb152d19
Luluh97 <luluadel.97@gmail.com>
2019-10-28 19:58:18 +0300

    commit

---
+++ Java/Fundamentals/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("My name is Luluh");
+        System.out.println("I’m 21 years old");
+        System.out.println("My hometown is Dammam, Saudi Arabia");
+ 
+    }
+}
