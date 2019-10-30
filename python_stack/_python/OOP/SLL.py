c9d59041ca6f6198e81af965a6c81763082e27ca
Luluh97 <luluadel.97@gmail.com>
2019-10-30 14:43:32 +0300

    commit

--- python_stack/_python/OOP/SLL.py
+++ python_stack/_python/OOP/SLL.py
@@ -28,6 +28,23 @@ class SList:
             print(current.data)
             current = current.next
         return self
+    
+    def remove_node(self): 
+		if self.head != None: 
+			current_node = self.head 
+		#Remove if list only has one node
+		if self.head.next == None: 
+			self.head = None
+			print self.head
+			#While not last, set current_node to next node 
+			while current_node.next != None: 
+				current_node = current_node.next
+				#If only one item in list, remove it
+				if current_node.next.next == None: 
+					current_node.next = None
+		else: 
+			print "No nodes"
+			return False
 
 mylist = SList()
 mylist.add_to_front(1).add_to_front(2).add_to_front(3).add_to_back(5).add_to_front(6).print_values()
