94b49ef1a2958b11dd794070c76b881512c6368c
Luluh97 <luluadel.97@gmail.com>
2019-10-30 12:25:23 +0300

    commit

--- Java/OOP/PorjectTest.java
+++
@@ -1,7 +0,0 @@
-class ProjectTest {
-    public static void main(String[] args) {
-        Vehicle a = new Project("Project1");
-        String name = a.getName();
-        System.out.println("The First Name is: " + name);
-    }
-}
