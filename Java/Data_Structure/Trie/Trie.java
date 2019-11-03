1f2358216e8c78099d6d48eec40dff7f0d0b3a97
Luluh97 <luluadel.97@gmail.com>
2019-11-03 08:42:31 +0300

    trie

--- Java/Data_Structure/Trie/Trie.java
+++ Java/Data_Structure/Trie/Trie.java
@@ -8,14 +8,9 @@ public class Trie {
 	    }
 	    
 	    public void insertWord(String word) {
-	        // gets the root node;
 	        Node currentNode = this.root;
-	        
-	        // iterates over every character in the word
 	        for(int i = 0; i < word.length(); i++) {
-	            // currentLetter is just the character / letter at the iteration
 	            Character currentLetter = word.charAt(i);
-	            // ask if the current letter is in the map of the current node
 				Node child = currentNode.children.get(currentLetter);
 	            if(child == null) {
 	                child = new Node();
@@ -29,27 +24,38 @@ public class Trie {
 		
 	boolean isPrefixValid(String prefix){
 			Node currentNode1 = this.root;
-			for(int i = 0; i < prefix.length(); i++) {
+			for(int i = 0; i < 1; i++) {
 				Character currentLetter1 = prefix.charAt(i);
 				Node child1 = currentNode1.children.get(currentLetter1);
 					if(child1 == null){
 						return false;
-					}		
+					}	
+					currentNode1 = 	child1;
+			}
+			return true;
+		}
+	
+	boolean isWordValid(String word){
+			Node currentNode1 = this.root;
+			for(int i = 0; i < word.length(); i++) {
+				Character currentLetter1 = word.charAt(i);
+				Node child1 = currentNode1.children.get(currentLetter1);
+					if(child1 == null){
+						return false;
+					}	
+					currentNode1 = 	child1;
 			}
 			return true;
 		}
 
 		
 
-	    // public void printkeys (Trie t) {
-	    // 	Set <Character> keys = t.root.children.keySet();
-	    // 			for ( Character key : keys) {
-		// 				System.out.println(key);
-		// 				Set <Character> keys1 = t.root.children.get(key).children.keySet();
-		// 				System.out.println("others..."+key1);
-
-	    				
-	    // 			}
-	    	
-	    // }
+	    public void printkeys () {
+		
+	    	Set <Character> keys = this.root.children.keySet();
+	    			for ( Character key : keys) {
+						System.out.println(key);
+			
+					}			
+	    }
 	    }
--- Java/Data_Structure/Trie/test.java
+++ Java/Data_Structure/Trie/test.java
@@ -2,16 +2,15 @@
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		   Trie trie = new Trie();
 	        trie.insertWord("car");
 	        trie.insertWord("card");
 	        trie.insertWord("chip");
 	        trie.insertWord("trie");
 			trie.insertWord("try");
-			System.out.println(trie.isPrefixValid("h"));
-			
-	        // trie.printkeys(trie);
+			System.out.println(trie.isPrefixValid("c"));
+			System.out.println(trie.isWordValid("try"));
+	        trie.printkeys();
 	}
 
 }
