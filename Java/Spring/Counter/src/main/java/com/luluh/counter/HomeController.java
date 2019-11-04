67ffff6767a5cefa320ef1cd95fe84e02624a6ca
Luluh97 <luluadel.97@gmail.com>
2019-11-04 22:33:38 +0300

    counter

---
+++ Java/Spring/Counter/src/main/java/com/luluh/counter/HomeController.java
@@ -0,0 +1,35 @@
+package com.luluh.counter;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	 @RequestMapping("/")
+	 public String index(HttpSession session) {
+		 if(session.getAttribute("count") == null) {
+	        	
+	        	session.setAttribute("count", 0);
+	        }
+	        else {
+	        	int count = (Integer) session.getAttribute("count");
+	        	count ++;
+	        	session.setAttribute("count", count);
+	        }
+		 
+		 return "index.jsp";
+	 }
+	 
+	 @RequestMapping("/counter")
+	 public String counter(HttpSession session){
+	        
+	    
+	       
+	        
+	        return "counter.jsp";
+	    }
+	
+}
